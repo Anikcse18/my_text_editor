12e4d9b1d60ff781f95b2f91066258aa07c673d7
Anikcse18 <istiakjavedanik@gmail.com>
2024-03-11 17:44:23 +0600

    Added alignment property and decoration property

--- js/textstyleB_I_U.js
+++ js/textstyleB_I_U.js
@@ -1,1 +1,48 @@
-console.log("Istiak");
+// function getInputField(textInputId) {
+//   const inputFieldvalue = document.getElementById(textInputId).innerHTML;
+//   return inputFieldvalue;
+// }
+
+var textarea = document.getElementById("textArea");
+
+//Text Bold Italic Underline
+// make text bold
+document.getElementById("boldText").addEventListener("click", function () {
+  textarea.classList.toggle("bold");
+});
+
+//make text ittalick
+document.getElementById("italicText").addEventListener("click", function () {
+  textarea.classList.toggle("italic");
+});
+
+//make text Underline
+document.getElementById("underlineText").addEventListener("click", function () {
+  textarea.classList.toggle("underline");
+});
+
+// Text Alignment issues
+
+//Text Left
+document.getElementById("alignLeft").addEventListener("click", function () {
+  textarea.classList.toggle("left");
+});
+
+//Text Center
+document.getElementById("alignMiddle").addEventListener("click", function () {
+  textarea.classList.toggle("center");
+});
+
+//Text Right
+document.getElementById("alignRight").addEventListener("click", function () {
+  textarea.classList.toggle("right");
+});
+
+//Text Justify
+document.getElementById("alignJustify").addEventListener("click", function () {
+  textarea.classList.toggle("justify");
+});
+
+
+
+
