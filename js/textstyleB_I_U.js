6be0295dba8a76dbac1441a1ffc9f5e55d90c32b
Anikcse18 <istiakjavedanik@gmail.com>
2024-03-12 01:44:09 +0600

    added all style

--- js/textstyleB_I_U.js
+++ js/textstyleB_I_U.js
@@ -43,6 +43,19 @@ document.getElementById("alignJustify").addEventListener("click", function () {
   textarea.classList.toggle("justify");
 });
 
+// Text Size change
+document.getElementById("fontSize").addEventListener("input", function () {
+  var fontSizer = document.getElementById("fontSize");
+  textarea.style.fontSize = fontSizer.value + "px";
+});
 
+//Text uppercase and lowercase
+document.getElementById("capitalSmall").addEventListener("click", function () {
+  textarea.classList.toggle("uppercase");
+});
 
-
+//Text Color
+document.getElementById("colorpicker").addEventListener("input", function () {
+  var colorValue = document.getElementById("colorpicker");
+  textarea.style.color = colorValue.value;
+});
